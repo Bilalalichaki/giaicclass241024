90bf128316e5fc9dac250d01e3fc9be223092e98
Bilal Ali <bilalalichaki@gmail.com>
2024-10-24 11:54:47 +0500

    class 24-10-24

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home () {
+  return (
+    <div className="bg-[url('/images/bg.jpeg')] p-[52px] bg-no-repeat bg-cover">
+    <h2 className="my-[100px] text-[36px] text-primary">Hello World</h2>
+    </div>
+    )
+};
